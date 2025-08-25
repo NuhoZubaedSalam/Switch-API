edcdc88ad5f4280548e631f9d8081bb5bf8c4b90
NuhoZubaedSalam <zs.nuho@gmail.com>
2025-08-25 13:52:34 +0200

    modified JS w/display functions

--- js/script.js
+++ js/script.js
@@ -1,20 +1,8 @@
-const loadUsers = () => {
-    fetch('https://randomuser.me/api/?results=200')
-        .then(response => response.json())
-        .then(users => console.log(users))
-}
-
-const loadCountries = () => {
-    fetch('https://restcountries.com/v3.1/independent?status=true')
-        .then(response => response.json())
-        .then(countries => console.log(countries))
-}
-
-const usersButton = document.getElementById('users');
-const countriesButton = document.getElementById('countries');
-
 document.querySelector("#buttons").addEventListener("click", (event) => {
-    if (event.target.id == 'users') {
+    const usersButton = document.getElementById('users');
+    const countriesButton = document.getElementById('countries');
+    
+    if (event.target.id === 'users') {
         usersButton.classList.add('selected-button'); 
         countriesButton.classList.remove('selected-button');
 
@@ -26,3 +14,27 @@ document.querySelector("#buttons").addEventListener("click", (event) => {
         loadCountries();
     }
 })
+
+const loadUsers = () => {
+    fetch('https://randomuser.me/api/?results=200')
+        .then(response => response.json())
+        .then(users => displayUsers(users))
+}
+
+const loadCountries = () => {
+    fetch('https://restcountries.com/v3.1/independent?status=true')
+        .then(response => response.json())
+        .then(countries => displayCountries(countries))
+}
+
+const displayCountries = (countries) => {
+    countries.forEach(country => {
+        console.log(country);
+    });
+}
+
+const displayUsers = (users) => {
+    users.forEach(user => {
+        console.log(user);
+    });
+}
