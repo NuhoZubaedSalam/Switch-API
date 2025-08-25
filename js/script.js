83edb294765f943d1baa6cc4c14175e3b2fda220
NuhoZubaedSalam <zs.nuho@gmail.com>
2025-08-25 09:04:33 +0200

    button switching enabled

--- js/script.js
+++ js/script.js
@@ -1,4 +1,28 @@
-document.getElementById("navbar").addEventListener("click", (event) => {
-    console.log(event.target.tagName);
-    
+const loadUsers = () => {
+    fetch('https://randomuser.me/api/?results=200')
+        .then(response => response.json())
+        .then(users => console.log(users))
+}
+
+const loadCountries = () => {
+    fetch('https://restcountries.com/v3.1/independent?status=true')
+        .then(response => response.json())
+        .then(countries => console.log(countries))
+}
+
+const usersButton = document.getElementById('users');
+const countriesButton = document.getElementById('countries');
+
+document.querySelector("#buttons").addEventListener("click", (event) => {
+    if (event.target.id == 'users') {
+        usersButton.classList.add('selected-button'); 
+        countriesButton.classList.remove('selected-button');
+
+        loadUsers();
+    } else {
+        countriesButton.classList.toggle('selected-button'); 
+        usersButton.classList.remove('selected-button');
+
+        loadCountries();
+    }
 })
